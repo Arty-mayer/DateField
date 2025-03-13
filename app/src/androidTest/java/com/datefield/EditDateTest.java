7926f25b4b6fd40af31a3b6b37514fb1aef49486
Artem Mayer <mayer.arty@gmail.com>
2025-03-13 21:18:22 +0100

    Initial commit

---
+++ app/src/androidTest/java/com/datefield/EditDateTest.java
@@ -0,0 +1,42 @@
+package com.datefield;
+
+import static androidx.test.InstrumentationRegistry.getTargetContext;
+import static org.junit.Assert.assertEquals;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class EditDateTest {
+
+    @Test
+    public void testMakeDigits() {
+
+    }}
+
+//        EditDate editDate = new EditDate(getTargetContext());
+////        editDate.chars[0] = '1';
+////        editDate.chars[1] = '2';
+////        editDate.chars[2] = '3';
+////        editDate.chars[3] = '4';
+////        editDate.chars[4] = '5';
+////        editDate.chars[5] = '6';
+////        editDate.chars[6] = '7';
+////        editDate.chars[7] = '8';
+////        editDate.chars[8] = '9';
+////        editDate.chars[9] = '1';
+//
+//        int result1 = editDate.makeDigits(0,4);
+//        int result2 = editDate.makeDigits(0,2);
+//        int result3 = editDate.makeDigits(7,2);
+//        int result4 = editDate.makeDigits(2 ,2);
+//
+//        assertEquals(1234, result1);
+//        assertEquals(12, result2);
+//        assertEquals(89, result3);
+//        assertEquals(34, result4);
+//    }
+//}
